47ca916e000a54fe9219d1564a2e249c32e83ab0
Narmadha sri <128790013+NarmadhaSri126@users.noreply.github.com>
2023-08-25 17:22:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    // Add smooth scrolling to all links
+    $("a").on("click", function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
+
+            // Store hash
+            var hash = this.hash;
+
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $("html, body").animate(
+                {
+                    scrollTop: $(hash).offset().top,
+                },
+                800,
+                function () {
+                    // Add hash (#) to URL when done scrolling (default click behavior)
+                    window.location.hash = hash;
+                }
+            );
+        } // End if
+    });
+});
+
+const scriptURL='https://script.google.com/macros/s/AKfycby9RQCj_pDpD_NmvT7DY7m-gO1KADL6DnWC1wFgsc8F1fG44gqQ475bZrd8IYFX3goVQQ/exec'
+const form= document.forms['submit-to-google-sheet']
+const msg=document.getElementById("me")
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML=""
+        },5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
